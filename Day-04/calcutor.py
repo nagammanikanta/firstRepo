0ff8808b3ee21ef12403d4af7a60ef1b12721153
nagammanikanta <nagammanikanta@gmail.com>
2023-10-26 15:07:09 +0530

    init

---
+++ Day-04/calcutor.py
@@ -0,0 +1,19 @@
+def addition(num1,num2):
+    add = num1 + num2
+    return add
+def sub(num1,num2):
+    s = num1 -num2
+    return s
+def mul(num1,num2):
+    m =num1 * num2
+    return m
+def div(num1,num2):
+    d = num1 % num2
+    return d
+addition(10,5)
+
+sub(10,5)
+
+mul(10,5)
+
+div(10,5)
