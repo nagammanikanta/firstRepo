b680b2ce63984d3d915805a93f3b28419158c4f7
nagammanikanta <nagammanikanta@gmail.com>
2023-10-26 16:29:26 +0530

    init

--- Day-04/gcs.py
+++ Day-04/gcs.py
@@ -1,14 +1,22 @@
-# Imports the Google Cloud client library
+import sys
 from google.cloud import storage
+import os
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "key.json"
+
+def create_bucket(bucket_name):
+    """Creates a new bucket."""
+    # bucket_name = "your-new-bucket-name"
+
+    storage_client = storage.Client()
+
+    bucket = storage_client.create_bucket(bucket_name)
+
+    print(f"Bucket {bucket.name} created")
 
 
-# Instantiates a client
-storage_client = storage.Client()
+# [END storage_create_bucket]
 
-# The name for the new bucket
-bucket_name = "mani-gcs-dev"
+if __name__ == "__main__":
+    create_bucket(bucket_name=sys.argv[1])
 
-# Creates the new bucket
-bucket = storage_client.create_bucket(bucket_name)
-
-print(f"Bucket {bucket.name} created.")
