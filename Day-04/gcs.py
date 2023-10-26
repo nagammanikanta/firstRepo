b1e857efd9ac4f547d9f5a4f79a1da06bdf2f825
nagammanikanta <nagammanikanta@gmail.com>
2023-10-26 16:58:17 +0530

    init

--- Day-04/gcs.py
+++ Day-04/gcs.py
@@ -1,7 +1,7 @@
 def implicit():
     from google.cloud import storage
     torage_client = storage.Client.from_service_account_json('mykey.json')
-    
+    bucket_name = "mani-gcs-dev"
 
     # If you don't specify credentials when constructing the client, the
     # client library will look for credentials in the environment.
