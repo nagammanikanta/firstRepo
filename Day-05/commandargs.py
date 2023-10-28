9d7847e9296962115efa78bc52fdde5f3bdf0fae
nagammanikanta <nagammanikanta@gmail.com>
2023-10-28 10:18:41 +0530

    init

---
+++ Day-05/commandargs.py
@@ -0,0 +1,30 @@
+import sys
+
+
+def add(num1, num2):
+    add = num1 + num2
+    return add
+def sub(num1, num2):
+    s = num1 - num2
+    return s
+def mul(num1, num2):
+    m =num1 * num2
+    return m
+
+num1 = print(sys.argv[1])
+operation = sys.argv[2]
+num2 = print(sys.argv[3])
+
+
+if operation == "add":
+    output = add(num1, num2)
+    print(output)
+    
+    
+if operation == "sub":
+    output = sub(num1, num2)
+    print(output)
+    
+if operation == "mul":
+    output = mul(num1, num2)
+    print(output)
