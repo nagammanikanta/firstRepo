cc8b0054b94c6f0fbd7c832cd6362b56ff4bf8ed
nagammanikanta <nagammanikanta@gmail.com>
2023-10-28 10:41:35 +0530

    init

--- Day-05/advcalculator.py
+++ Day-05/advcalculator.py
@@ -5,6 +5,10 @@ def div(num1, num2):
     d = num1 * num2
     return d
 
+num1 = float(sys.argv[1])
+operation = sys.argv[2]
+num2 = float(sys.argv[3])
+
 if operation == "div":
     output = div(num1, num2)
     print(output)
