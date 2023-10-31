c5377ba7cf0ab0d5199d4387b84adfa91738da93
nagammanikanta <nagammanikanta@gmail.com>
2023-11-01 05:24:18 +0530

    init

--- Day-06/armaticoperator.py
+++ Day-06/armaticoperator.py
@@ -1,5 +1,15 @@
-a = 5
+a = 6
 b = 3
-result = a + b
-print(result)
+
+sum_result = a + b
+difference_result = a - b
+product_result = a * b
+quotiend_result = a / b
+
+print("sum:", sum_result)
+print("difference:", difference_result)
+print("product:", product_result)
+print("quotiend:", quotiend_result)
+
+
  
