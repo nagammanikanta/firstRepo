3ac64a8531e5e7c7e5aeb20e471f7a0c0e48eae4
nagammanikanta <nagammanikanta@gmail.com>
2023-11-02 11:38:55 +0530

    init

---
+++ Day-07/test.py
@@ -0,0 +1,13 @@
+import sys
+type = sys.argv[1]
+
+if type =="t2.micro":
+    print("it will charge 2 dollers a day")
+elif type == "t2.medium":
+    print("it will charge 4 dollers a day")
+    
+elif type =="t2.xlarge":
+    print("it will charge 8 dollars a day")
+
+else:
+    print("please provide a valid instance type")
