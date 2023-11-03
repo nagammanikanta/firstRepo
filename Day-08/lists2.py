6fc25e1ba2f6a798549a5296395c43d3dbab513b
nagammanikanta <nagammanikanta@gmail.com>
2023-11-03 14:43:04 +0530

    init

--- Day-08/lists2.py
+++ Day-08/lists2.py
@@ -2,3 +2,6 @@ gcs_bucket_list = ["mani_demo_bucket", "abhi_demo_bucket", "john_demo_bucket", "
 gcs_bucket_list.append("main_demo_bucket")
 print(gcs_bucket_list)
 
+gcs_bucket_list.remove("mani_demo_bucket")
+
+print(gcs_bucket_list)
