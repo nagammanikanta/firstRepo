83eeeebd8d10cfed1b61de19d7e62c07b431be3b
nagammanikanta <nagammanikanta@gmail.com>
2023-11-03 15:20:05 +0530

    init

--- Day-08/tuple.py
+++ Day-08/tuple.py
@@ -7,3 +7,7 @@ print(len(gcs_bucket_list))
 print(gcs_bucket_list[0])
 
 print(gcs_bucket_list[0] + "  " + gcs_bucket_list[3])
+
+new_list = gcs_bucket_list[0:2]
+print(new_list)
+print(len(new_list))
