f35a87b96fb343e92e0bacdac61a0af9237ba504
nagammanikanta <nagammanikanta@gmail.com>
2023-11-16 09:53:55 +0530

    init

---
+++ Day-10/manifuc.py
@@ -0,0 +1,11 @@
+def main():
+    folder_paths = input("Enter the list of folder paths separated by spaces: ").split()
+    print(folder_paths)
+    
+    
+    # print elements in the list 
+    #for folder_path in folder_paths:
+    #  print(folder_path)
+    
+if __name__ == "__main__":
+    main()
