f774cd4bdfb7ac2e11d463985c67d7b19f008de2
nagammanikanta <nagammanikanta@gmail.com>
2023-11-21 15:37:38 +0530

    init

--- Day-11/set.py
+++ Day-11/set.py
@@ -7,3 +7,17 @@ print(my_set)
 
 my_set.remove(2)
 print(my_set)
+
+
+
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
+
+union_set = set1.union(set2)
+print(union_set)
+
+intersection_set = set1.intersection(set2)
+print(intersection_set)
+
+difference_set = set1.difference(set2)
+print(difference_set)
