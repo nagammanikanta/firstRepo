a92202358466a57c3af9b7c95633a54e14a59e92
nagammanikanta <nagammanikanta@gmail.com>
2023-11-21 16:18:54 +0530

    init

--- Day-11/set2.py
+++ Day-11/set2.py
@@ -9,3 +9,6 @@ print(server_config)
 def get_server_status(server_name):
     return server_config.get(server_name, {}).get('status', 'server not found')
 
+server_name = 'server2'
+status = get_server_status(server_name)
+print(f"{server_name} status: {status}")
