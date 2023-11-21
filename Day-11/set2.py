5d8cad1bd6f38c8cc2cced29fbb5f89344d3b921
nagammanikanta <nagammanikanta@gmail.com>
2023-11-21 16:11:51 +0530

    init

---
+++ Day-11/set2.py
@@ -0,0 +1,7 @@
+server_config = {
+    'server1':{'ip':'192.168.1.0', 'port':'8080', 'status':'active'},
+    'server2':{'ip':'192.168.1.1', 'port':'8081', 'status':'inactive'},
+    'server3':{'ip':'192.168.1.3', 'port':'9000', 'status':'active'}
+    
+}
+print(server_config)
