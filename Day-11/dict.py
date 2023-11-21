00fa92f642e9a5ec827b84c2099d0659423579c5
nagammanikanta <nagammanikanta@gmail.com>
2023-11-21 15:19:04 +0530

    init

--- Day-11/dict.py
+++ Day-11/dict.py
@@ -3,8 +3,12 @@ students_info = {
     'age' : '29',
     'role' :'devops'
 }
+print(students_info['name'])
 
+del students_info['age']
 
+if 'age' in students_info:
+    print('Age is present in the dictionary is 29')
 
 for key, value in students_info.items():
     print(key, value)
