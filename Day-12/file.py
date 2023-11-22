5bad425f7f82f63ae15096f1ba739b34ecab5119
nagammanikanta <nagammanikanta@gmail.com>
2023-11-22 15:47:10 +0530

    init

--- Day-12/file.py
+++ Day-12/file.py
@@ -10,3 +10,12 @@ def update_server_config(file_path, key, value):
         else:
                 # Keep the existing line as it is
                 file.write(line)
+# Path to the server configuration file
+server_config_file = 'server.conf'
+
+# Key and new value for updating the server configuration
+key_to_update = 'MAX_CONNECTIONS'
+new_value = '600'  # New maximum connections allowed
+
+# Update the server configuration file
+update_server_config(server_config_file, key_to_update, new_value)
