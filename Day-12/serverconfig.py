2c515b9b22fee7e119a7d091dc7f9cd7f093f4ef
nagammanikanta <nagammanikanta@gmail.com>
2023-11-22 15:52:53 +0530

    init

---
+++ Day-12/serverconfig.py
@@ -0,0 +1,17 @@
+# Server Configuration File
+
+# Network Settings
+PORT = 8080
+MAX_CONNECTIONS=600
+TIMEOUT = 30
+
+# Security Settings
+SSL_ENABLED = Truerue
+SSL_CERT = /path/to/certificate.pem
+
+# Logging Settings
+LOG_LEVEL = 
+LOG_FILE = /var/log/server.log
+
+# Other Settings
+ENABLE_FEATURE_X = True
