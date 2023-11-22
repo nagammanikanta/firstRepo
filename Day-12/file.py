5349194c9666e2f551c9acd8e297a0eb6e5ea0bb
nagammanikanta <nagammanikanta@gmail.com>
2023-11-22 15:48:38 +0530

    init

--- Day-12/file.py
+++ Day-12/file.py
@@ -1,15 +1,19 @@
 def update_server_config(file_path, key, value):
-      with open(file_path, 'r') as file:
+    # Read the existing content of the server configuration file
+    with open(file_path, 'r') as file:
         lines = file.readlines()
-      with open(file_path, 'w') as file:
+
+    # Update the configuration value for the specified key
+    with open(file_path, 'w') as file:
         for line in lines:
-     #Check if the line starts with the specified key
-          if key in line:
+            # Check if the line starts with the specified key
+            if key in line:
                 # Update the line with the new value
                 file.write(key + "=" + value + "\n")
-        else:
+            else:
                 # Keep the existing line as it is
                 file.write(line)
+
 # Path to the server configuration file
 server_config_file = 'server.conf'
 
