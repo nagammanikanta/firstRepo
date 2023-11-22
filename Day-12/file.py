b2a8c0f423a0783d2e8ddfa0f8cdc3593d58e26d
nagammanikanta <nagammanikanta@gmail.com>
2023-11-22 15:46:15 +0530

    init

--- Day-12/file.py
+++ Day-12/file.py
@@ -1,3 +1,12 @@
 def update_server_config(file_path, key, value):
       with open(file_path, 'r') as file:
         lines = file.readlines()
+      with open(file_path, 'w') as file:
+        for line in lines:
+     #Check if the line starts with the specified key
+          if key in line:
+                # Update the line with the new value
+                file.write(key + "=" + value + "\n")
+        else:
+                # Keep the existing line as it is
+                file.write(line)
